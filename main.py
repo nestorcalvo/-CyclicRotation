44c0f528e6d2c64ffc45bebaff71e8d5baf18682
nestorcalvo <Nestor98112612483>
2021-02-22 18:31:11 -0500

    Cyclic Rotation Code

---
+++ main.py
@@ -0,0 +1,19 @@
+def solution(A, K):
+    # write your code in Python 3.6
+    temp = A[:]
+    if K >= len(A) or K == 0:
+        K = K % len(A)
+        if K == 0:
+            return A
+    for i in range(len(A)):
+        if i < K:
+            temp[i] = A[len(A) + i - K]
+        else:
+            temp[i] = A[i - K]
+    return temp
+
+
+if __name__ == '__main__':
+    A = [2, 4, 6, 8]
+    K = 2
+    print(solution(A, K))
